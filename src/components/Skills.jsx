50fa644db4be8946932ab5d9923c55876ffce130
Shamant Myageri <code.shamant@gmail.com>
2025-03-13 17:48:22 +0530

    Added the skill section in the about page with all the animations.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,83 @@
 
 export default function Skills() {
+
     return (
-        <div>Skills</div>
+        <div className="w-full overflow-hidden">
+            <ul className=" flex animate-intifiteScroll hover:[animation-play-state:paused] w-max">
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/javascript.svg" alt="javascript.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/git.svg" alt="git.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/github.svg" alt="github.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/react.svg" alt="react.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/node.svg" alt="node.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/mongodb.svg" alt="mongodb.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/tailwindcss.svg" alt="tailwindcss.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/supabase.svg" alt="supabase.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/vitejs.svg" alt="vitejs.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/express.svg" alt="express.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/css.svg" alt="css.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/html.svg" alt="html.svg" />
+                </li>
+                {/* duplicates */}
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/javascript.svg" alt="javascript.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/git.svg" alt="git.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/github.svg" alt="github.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/react.svg" alt="react.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/node.svg" alt="node.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/mongodb.svg" alt="mongodb.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/tailwindcss.svg" alt="tailwindcss.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/supabase.svg" alt="supabase.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/vitejs.svg" alt="vitejs.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/express.svg" alt="express.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/css.svg" alt="css.svg" />
+                </li>
+                <li className=" scroll_item flex justify-center items-center aspect-square md:h-20 md:m-10 bg-softWhite rounded-full">
+                    <img className=" max-h-[70%] max-w-[70%]" src="/techLogo/html.svg" alt="html.svg" />
+                </li>
+            </ul>
+        </div>
     )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,23 @@
+import Skills from "../components/Skills";
 
 export default function About() {
     return (
         <div className=" text-softWhite flex flex-col justify-center items-center">
             <div className="h-screen w-full max-w-6xl flex justify-between mx-auto items-center px-4">
                 <div className=" font-montserrat">
-                    <h1 className=" font-poppins font-light text-[22px] py-2">more about me</h1>
-                    <p className=" font-semibold text-5xl py-2 leading-tight">Full-Stack Developer with <br /> a Knack for Everything <br /> Else!</p>
-                    <p className=" w-[550px] text-[16px] py-2 leading-snug">I'm Shamant, a Full-Stack developer passionate about building scalable web applications and solving complex problems. With experience in various projects like a blog site, real estate app, and eCommerce platform, I specialize in MERN stack, API development, and database management {"(SQL & NoSQL)"}.<br /><br />Currently, I’m diving into React Native for mobile development. I thrive on learning new technologies and collaborating on impactful projects. Let’s connect and build something amazing!<br /><br />Because the best way to learn is by building — and that’s exactly what I do.</p>
+                    <h1 className=" font-poppins font-light text-[1.375rem] my-2">
+                        more about me
+                    </h1>
+                    <p className=" font-semibold text-5xl my-2 leading-tight">
+                        Full-Stack Developer with <br /> a Knack for Everything <br /> Else!
+                    </p>
+                    <p className=" w-[550px] text-[16px] my-3 leading-snug">
+                        I'm Shamant, a Full-Stack developer passionate about building scalable web applications and solving complex problems. With experience in various projects like a blog site, real estate app, and eCommerce platform, I specialize in MERN stack, API development, and database management {"(SQL & NoSQL)"}.<br /><br />Currently, I’m diving into React Native for mobile development. I thrive on learning new technologies and collaborating on impactful projects. Let’s connect and build something amazing!<br /><br />Because the best way to learn is by building — and that’s exactly what I do.
+                    </p>
                     <button className=" 
-                    font-poppins bg-softWhite rounded-3xl w-40 h-10 text-slate-800 mt-7 mb-2
+                    font-poppins bg-softWhite rounded-3xl w-40 h-10 text-slate-800 mt-6 mb-2
                     hover:bg-electricBlue active:scale-95 active:opacity-80 
-                    transition duration-200 ease-in-out  ">
+                    transition duration-200 ease-in-out">
                         Download CV
                     </button>
                 </div>
@@ -18,8 +25,15 @@ export default function About() {
                     image
                 </div>
             </div>
-            <div className=" h-screen">
-                what i work with
+            <div className=" h-screen w-full max-w-6xl px-4 flex flex-col  justify-center">
+                <h2 className=" font-poppins font-light text-[1.375rem] my-14">what I work with?</h2>
+                <Skills />
+                <button className=" 
+                font-poppins bg-softWhite rounded-3xl w-40 h-10 text-slate-800 my-14
+                hover:bg-neonYellow active:scale-95 active:opacity-80 
+                transition duration-200 ease-in-out  ">
+                    Hire Me
+                </button>
             </div>
         </div>
     )
